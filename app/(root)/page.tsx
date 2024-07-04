92aa43e6116f241d94fc9409fdd16e5d50494673
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-07-04 09:22:36 +0600

    Side bar done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1 className={cn("text-4xl font-bold")}>ArtisAI</h1>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton showName afterSwitchSessionUrl="/" />
     </div>
   );
 };
---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { navLinks } from "@/constants";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <div className="sidebar">
+      <div className="flex flex-col size-full gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = pathname === link.route;
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
+                    <Link className="sidebar-link" href={link.route}><Image src={link.icon} alt={link.label} width={24} height={24} className={`${isActive && "brightness-200"}`} />{link.label}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = pathname === link.route;
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
+                    <Link className="sidebar-link" href={link.route}><Image src={link.icon} alt={link.label} width={24} height={24} className={`${isActive && "brightness-200"}`} />{link.label}</Link>
+                  </li>
+                );
+              })}
+
+              <li className="flex-center p-4 gap-2 cursor-pointer">
+                <UserButton  showName afterSwitchSessionUrl="/" />
+              </li>
+            </ul>
+
+
+          </SignedIn>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
