204a70a6b93b65e2c1b90bb9f02639f4640bfca2
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-08-11 09:19:53 +0600

    Middleware some code changed for public route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
-import {  createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
-export default createRouteMatcher(["/api/webhooks/clerk"]);
+const isPublicRoute = createRouteMatcher(["/",'/api/webhooks/clerk','/sign-in(.*)', '/sign-up(.*)']);
+export default clerkMiddleware((auth, request) =>{
+  if(!isPublicRoute(request)){
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
